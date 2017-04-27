8764846ae88b0aeb73958cada8c94d10eafe0066
C-Rodg <rodgersbc@gmail.com>
2017-04-26 17:22:13 -0700

    Completed boilerplate web extension

--- extension/extractor.js
+++ extension/extractor.js
@@ -4,7 +4,6 @@
     anchors.forEach((a) => {
         as.push({
             href: a.href,
-            rel: a.rel,
             text: a.textContent
         });
     });
--- extension/popup.js
+++ extension/popup.js
@@ -1,17 +1,29 @@
 let links = document.getElementById('links');
+console.log(links);
 
 chrome.tabs.executeScript(null, {
     file: 'extractor.js'
 }, (results) => {
-    let anchors = results[0];
-    anchors.forEach(function(a) {
-        const tr = `<tr>
-                        <td><a href="${a.href}">${a.href}</a><br />
-                            ${a.text}
-                        </td>
-                        <td>${a.rel}</td>
-                    </tr>
-                    `;
-        links.appendChild(tr);
-    });
+    console.log(results);
+    if (results) {
+        let anchors = results[0];
+        anchors.forEach(function(a) {
+            let tr = document.createElement('tr'),
+                td_text = document.createElement('td'),
+                td_url = document.createElement('td'),
+                link = document.createElement('a');
+
+            link.href = a.href;
+            link.target = "_blank";
+            link.textContent = a.href;
+
+            td_url.appendChild(link);
+            td_text.textContent = a.text;
+
+            tr.appendChild(td_text);
+            tr.appendChild(td_url);
+
+            links.appendChild(tr);
+        });
+    }    
 });
